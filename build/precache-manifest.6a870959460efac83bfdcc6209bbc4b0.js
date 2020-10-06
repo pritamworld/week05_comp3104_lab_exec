20e82322476c672ada1f6c4a124cb1f10b8e9d99
iamkarin <karin@walla.com>
2020-10-06 01:26:08 -0400

    DevOps Week05 Exercise - Scripts

---
+++ build/precache-manifest.6a870959460efac83bfdcc6209bbc4b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18e6a82a637966365a17",
+    "url": "/static/css/main.13014cbf.chunk.css"
+  },
+  {
+    "revision": "18e6a82a637966365a17",
+    "url": "/static/js/main.18e6a82a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "f1fd9f0d3b7f6ffdb8ab77fcdbfd71ec",
+    "url": "/index.html"
+  }
+];
