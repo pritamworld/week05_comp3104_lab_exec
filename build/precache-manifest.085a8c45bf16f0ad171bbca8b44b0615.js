913294a84036465f1d940b4f38326e4a8d5c6f5d
Bruna Abatepaulo <bruna.abatepaulo@hotmail.com>
2020-10-06 11:16:26 -0300

    revised commit

---
+++ build/precache-manifest.085a8c45bf16f0ad171bbca8b44b0615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0f99a1445b0498a4ad0",
+    "url": "/static/css/main.8eeb12a6.chunk.css"
+  },
+  {
+    "revision": "b0f99a1445b0498a4ad0",
+    "url": "/static/js/main.b0f99a14.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "1374c8b88338f921076186eefa1f6a33",
+    "url": "/index.html"
+  }
+];
