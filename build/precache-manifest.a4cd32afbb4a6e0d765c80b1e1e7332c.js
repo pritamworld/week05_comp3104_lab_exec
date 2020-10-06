d8e5cf75b46a5e5ae6c0579ca93775a60a508744
SabihJ <53152643+SabihJ@users.noreply.github.com>
2020-10-06 19:33:35 -0400

    first change

---
+++ build/precache-manifest.a4cd32afbb4a6e0d765c80b1e1e7332c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e74b36a84101091267a",
+    "url": "/static/css/main.618b0bec.chunk.css"
+  },
+  {
+    "revision": "5e74b36a84101091267a",
+    "url": "/static/js/main.5e74b36a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "2f19f82153ba0b0bf892fa542cf85e0f",
+    "url": "/index.html"
+  }
+];
