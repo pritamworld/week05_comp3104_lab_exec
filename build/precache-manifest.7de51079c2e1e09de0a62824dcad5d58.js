50eddb1c6582ae869286dd0eda035f626d197e91
gbc343 <gordonwells@hotmail.com>
2020-10-05 20:51:47 -0400

    Branch updated

---
+++ build/precache-manifest.7de51079c2e1e09de0a62824dcad5d58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9060b9bdce8d70c312e",
+    "url": "/static/css/main.98f01681.chunk.css"
+  },
+  {
+    "revision": "d9060b9bdce8d70c312e",
+    "url": "/static/js/main.d9060b9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "1c3f8820610b2eae09edd8e551fa0474",
+    "url": "/index.html"
+  }
+];
