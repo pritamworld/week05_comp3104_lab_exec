fa36a1aed5d8b52141f921389d64ace1fbd630ff
Thinh Tran <thinhtran1769@gmail.com>
2020-10-06 19:35:41 -0400

    this is a message

---
+++ build/precache-manifest.90a3e49cbcc2477a23fa1ae4df0c2899.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3362b85b9b2e6307035f",
+    "url": "/static/css/main.2a410433.chunk.css"
+  },
+  {
+    "revision": "3362b85b9b2e6307035f",
+    "url": "/static/js/main.3362b85b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "c92c0034210e9b880a272f65f1d7ec5b",
+    "url": "/index.html"
+  }
+];
