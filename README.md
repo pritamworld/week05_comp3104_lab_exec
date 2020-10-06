c76b2dcc4ea60880bb1af7bcb37c60a9ff390623
Vincent-hide <hidehiro.okayama@gmail.com>
2020-10-05 20:27:45 -0400

    week 5

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dev-ops week5
+
+[![Build Status](https://travis-ci.org/Vincent-hide/week05_comp3104_lab_exec.svg?branch=master)](https://travis-ci.org/Vincent-hide/week05_comp3104_lab_exec)
---
+++ build/precache-manifest.afd548c03eede8449146889453d37d2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a88d64338a2e159df9be",
+    "url": "/static/css/main.57351401.chunk.css"
+  },
+  {
+    "revision": "a88d64338a2e159df9be",
+    "url": "/static/js/main.a88d6433.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "a96c4d288cadfdc552e9dbcff0dd5286",
+    "url": "/index.html"
+  }
+];
