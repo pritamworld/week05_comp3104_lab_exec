9eb46c200b958ca060d8f962e8abf7d88fd3f957
KevinUfkes <62268325+KevinUfkes@users.noreply.github.com>
2020-10-05 17:31:22 -0700

    added project files to repository

---
+++ build/precache-manifest.53e7ac0a63aaee6f70b742b0f5f9ce86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "215c468d601762c262df",
+    "url": "/static/css/main.36339a93.chunk.css"
+  },
+  {
+    "revision": "215c468d601762c262df",
+    "url": "/static/js/main.215c468d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "8c92af0890b7be4b7f2e41da9608bbc1",
+    "url": "/index.html"
+  }
+];
