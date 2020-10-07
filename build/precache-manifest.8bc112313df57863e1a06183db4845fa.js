d7fb995525c801cccbe1d991beab4f5c287dd8dd
Henok H Mengesha <55731489+kifle1991@users.noreply.github.com>
2020-10-07 19:50:05 -0400

    it is committed

---
+++ build/precache-manifest.8bc112313df57863e1a06183db4845fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a0b5cb22b33dd6ff81a",
+    "url": "/static/css/main.5769b9d2.chunk.css"
+  },
+  {
+    "revision": "4a0b5cb22b33dd6ff81a",
+    "url": "/static/js/main.4a0b5cb2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "199589d97828958d452f74c43f0232d1",
+    "url": "/index.html"
+  }
+];
