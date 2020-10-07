9d4629a3d02679dd45a37c390079ac811d43b7e2
Rafed Fareed <ibardock@yahoo.ca>
2020-10-06 20:38:32 -0700

    new files added

---
+++ build/precache-manifest.5a2682bd798b33bd213697a93d6a690c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "daaddb606341b69d157b",
+    "url": "/static/css/main.a7a3f788.chunk.css"
+  },
+  {
+    "revision": "daaddb606341b69d157b",
+    "url": "/static/js/main.daaddb60.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "30c28ccc21e9b7fd26cb9e7d0d88dd15",
+    "url": "/index.html"
+  }
+];
