9994babb7ae21bf40b8379f7c2ccde9497ab9f70
linminca <min-lin@hotmail.com>
2020-10-06 23:48:14 -0400

    added project dependencies to our package.json

---
+++ build/precache-manifest.228d6905968e3652441c6f642da1a566.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d667e879228a63e4b85f",
+    "url": "/static/css/main.80ff0dcf.chunk.css"
+  },
+  {
+    "revision": "d667e879228a63e4b85f",
+    "url": "/static/js/main.d667e879.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "f7c31270400f24e4527f367d6d8e96d3",
+    "url": "/index.html"
+  }
+];
