ae87f631cfa1c33116480133699bcddb6ef681a2
Rachel Titco <rachel.titco@gmail.com>
2020-10-06 23:54:26 -0400

    adding all changes at once which includes package.json work and app src

---
+++ build/precache-manifest.3765322886fea41ab1b36f378187f867.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "938244a692354d1c0024",
+    "url": "/static/css/main.cb38cb38.chunk.css"
+  },
+  {
+    "revision": "938244a692354d1c0024",
+    "url": "/static/js/main.938244a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "9c26b2f5079a5acf395700b1315b637f",
+    "url": "/index.html"
+  }
+];
