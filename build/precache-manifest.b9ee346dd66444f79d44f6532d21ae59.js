0961179eb988df70b8fb562a7e215fac9735b338
joelpiovesan <joelpiovesandesigns@gmail.com>
2020-10-06 23:55:29 -0400

    including application source

---
+++ build/precache-manifest.b9ee346dd66444f79d44f6532d21ae59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43f3902f6174696cb47a",
+    "url": "/static/css/main.3ace504a.chunk.css"
+  },
+  {
+    "revision": "43f3902f6174696cb47a",
+    "url": "/static/js/main.43f3902f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "d12254a7f218806373f0f8537e548794",
+    "url": "/index.html"
+  }
+];
