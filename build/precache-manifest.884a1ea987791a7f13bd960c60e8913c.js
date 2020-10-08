a0f71d7cc88bdd4af1ed38da6819d91b88e12e7e
Suvash Sharma <suvayeti@gmail.com>
2020-10-07 20:29:27 -0400

    Committed all

---
+++ build/precache-manifest.884a1ea987791a7f13bd960c60e8913c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e467aac0421e5b8ef00f",
+    "url": "/static/css/main.dc92bba3.chunk.css"
+  },
+  {
+    "revision": "e467aac0421e5b8ef00f",
+    "url": "/static/js/main.e467aac0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "c195b9bdf311ed1615fba6b450e759b3",
+    "url": "/index.html"
+  }
+];
