55d22826dbc503c9e7c9a4085aade9c368c609a2
Hyunjun Yoon <yhj940719@gmail.com>
2020-10-08 09:33:12 -0400

    added all new files

---
+++ build/precache-manifest.bef83f2baef1cda77772d57e63824766.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "106378d6e240b6254129",
+    "url": "/static/css/main.99d6ae4f.chunk.css"
+  },
+  {
+    "revision": "106378d6e240b6254129",
+    "url": "/static/js/main.106378d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "8310e27ff10d93137ec0ab9b7ed788b5",
+    "url": "/index.html"
+  }
+];
