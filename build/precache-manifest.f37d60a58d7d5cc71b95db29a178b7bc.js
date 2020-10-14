5bbeee1c313b7a7b62b92007b21aed584be3f046
Nick Chinsen <nicholaschinsen@gmail.com>
2020-10-14 18:56:01 -0400

    Added build folder

---
+++ build/precache-manifest.f37d60a58d7d5cc71b95db29a178b7bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0dc8dd7ade182c90eb8",
+    "url": "/static/css/main.afdf854e.chunk.css"
+  },
+  {
+    "revision": "e0dc8dd7ade182c90eb8",
+    "url": "/static/js/main.e0dc8dd7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "43606ef56a5f9efbf299dc5724201f57",
+    "url": "/index.html"
+  }
+];
