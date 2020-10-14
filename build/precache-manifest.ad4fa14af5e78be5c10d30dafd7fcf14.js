2e90ac193089bb0f546be0331c9e39b7a3302960
Dhyanpatel065 <dhyan.patel065@gmail.com>
2020-10-13 22:02:52 -0400

    I did this four time and i finally got successgit status!

---
+++ build/precache-manifest.ad4fa14af5e78be5c10d30dafd7fcf14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "791ffd98549c29d7e34a",
+    "url": "/static/css/main.fcf4ebe6.chunk.css"
+  },
+  {
+    "revision": "791ffd98549c29d7e34a",
+    "url": "/static/js/main.791ffd98.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "d3c2e6f8a71964546270470193ff7bf9",
+    "url": "/index.html"
+  }
+];
