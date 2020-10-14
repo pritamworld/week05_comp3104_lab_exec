6eb5cab4dd7e491461726fb5b85cdaa2ce46401c
farahmand.1990 <farahmand.1990@gmail.com>
2020-10-14 19:00:20 -0400

    update

---
+++ build/precache-manifest.ebbe5630d8230c9df54bd6663c3d70b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44c6f66de1c02045f02c",
+    "url": "/static/css/main.b261edaf.chunk.css"
+  },
+  {
+    "revision": "44c6f66de1c02045f02c",
+    "url": "/static/js/main.44c6f66d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "e3fa5594a8a49dfb32396e1837314e5a",
+    "url": "/index.html"
+  }
+];
