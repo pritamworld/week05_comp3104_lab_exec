8a90a8764695119b754c1121a219f224ddee6139
alexbalez <alexbalez@gmail.com>
2020-10-17 14:39:35 -0400

    build files

---
+++ build/precache-manifest.9d3411798ff26310ada580f196b8146a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2579ebc434e45eba4fc",
+    "url": "/static/css/main.34a321e4.chunk.css"
+  },
+  {
+    "revision": "d2579ebc434e45eba4fc",
+    "url": "/static/js/main.d2579ebc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "bb88a0d3dd5ef5044842a22cd337bd5b",
+    "url": "/index.html"
+  }
+];
